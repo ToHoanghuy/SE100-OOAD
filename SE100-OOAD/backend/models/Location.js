315f411234f7fe1cc502f53678dc83ad55d92fd6
thinh_nguyen_phuc <113797076+timothyagile@users.noreply.github.com>
2024-12-25 15:08:38 +0700

    add slug field to location model and update location search by slug

--- SE100-OOAD/backend/models/Location.js
+++ SE100-OOAD/backend/models/Location.js
@@ -23,6 +23,7 @@ const locationSchema = new Schema({
         type: String,
         required: true,
     },
+    slug: {type: String, required: true},
     description: {
         type: String,
         //required: true,

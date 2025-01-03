882756fa3e948dfbc6c1c859ed6e5f832b181976
TTMTN <22520936@gm.uit.edu.vn>
2025-01-03 12:37:27 +0700

    Update

--- SE100-OOAD/web-app/src/pagesofbusiness/BusinessDetailBookingScreen.js
+++ SE100-OOAD/web-app/src/pagesofbusiness/BusinessDetailBookingScreen.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import '../styles/DetailBookingScreen.css';
 import { FaBell, FaCalendar, FaEnvelope, FaMoneyBill } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import pagination from '../components/Pagination';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhoneAlt, faEnvelope, faUser, faMapMarkerAlt, faMemo, faLocation } from '@fortawesome/free-solid-svg-icons';
-
+import moment from 'moment';
 
 
 const BusinessDetailBookingScreen = () => {
     
+    const { id: bookingId } = useParams();
     const [currentTab, setCurrentTab] = useState('customerinfo');
+    const [userData, setUserData] = useState(null);
+    const [locationData, setLocationData] = useState(null);
+    const [userOfBookingId, setUserOfBookingId] = useState(null);
+    const [booking, setBooking] = useState(null);
+    const [loading, setLoading] = useState(true); 
+    const [error, setError] = useState(null);
+
+    const formattedDate = booking?.dateBooking
+    ? moment(booking?.dateBooking).format('DD/MM/YYYY HH:mm:ss')
+    : 'Chưa có ngày đặt';
 
     const handleCustomerInfoClick =()=> {
         setCurrentTab('customerinfo');
@@ -19,6 +31,71 @@ const BusinessDetailBookingScreen = () => {
     const handleLocationInfoClick = () => {
         setCurrentTab('locationinfo')
     }
+    const fetchUserData = async () => {
+        try {
+            setLoading(true); 
+            const response = await fetch(`http://localhost:3000/user/getbyid/${userOfBookingId}`);
+            const data = await response.json();
+
+            if (response.ok) {
+                setUserData(data.data);
+                console.log('data of detail booking: ', data.data);
+            } else {
+                setError(data.message || 'Không thể lấy thông tin người dùng');
+            }
+        } catch (err) {
+            setError('Có lỗi xảy ra khi gọi API');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const fetchLocationData = async () => {
+        try {
+            setLoading(true); 
+            const response = await fetch(`http://localhost:3000/locationbyid/${booking.locationId}`);
+            const data = await response.json();
+
+            if (response.ok) {
+                setLocationData(data.data);
+                console.log('data of detail booking: ', data.data);
+            } else {
+                setError(data.message || 'Không thể lấy thông tin người dùng');
+            }
+        } catch (err) {
+            setError('Có lỗi xảy ra khi gọi API');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        const userOfBooking = localStorage.getItem('userOfBookingId');
+        if (userOfBooking) {
+            console.log('get booking from localstorage: ', userOfBooking);
+            setUserOfBookingId(userOfBooking);
+        }
+        const storedBooking = localStorage.getItem('selectedBooking');
+        if (storedBooking) {
+            console.log('get booking from localstorage: ', JSON.parse(storedBooking));
+            setBooking(JSON.parse(storedBooking));
+        }
+    }, [bookingId]);
+
+    // useEffect(() => {
+    //     if (bookingId) {
+    //         fetchUserData();
+    //     }
+    // }, [bookingId]);
+
+    useEffect(() => {
+        if (userOfBookingId) {
+            fetchUserData();
+        }
+        if (booking) {
+            fetchLocationData();
+        }
+    }, [userOfBookingId], [booking]);
     
     return (
         <div class="container">
@@ -30,25 +107,25 @@ const BusinessDetailBookingScreen = () => {
                             <img alt="Profile picture of a person" class="w-20 h-20 rounded-full mr-4" height="80" src="https://storage.googleapis.com/a1aa/image/0FPVWfLJ1m0nJS9YfULFrbvezZsDHus5bXhqxVDA6tO9UMKnA.jpg" width="80"/>
                             <div>
                                 <h1 class="text-xl font-bold">
-                                    Du lịch Hồ Cốc - Vũng Tàu
+                                    {booking?.locationName}
                                 </h1>
                                 <div class="flex items-center text-gray-600 mt-2">
                                     <FaCalendar class="mr-2"/>
                                   
-                                        <span>04/08/2024</span>
+                                        <span>{moment(booking?.checkinDate).format('DD/MM/YYYY HH:mm:ss')} - {moment(booking?.checkoutDate).format('DD/MM/YYYY HH:mm:ss')}</span>
                                     </div>
                                     <div class="flex items-center mt-2">
                                         <FaMoneyBill class="mr-2 w-5"/>
                                         
-                                        <span class="line-through text-gray-400">68,000,000 đ</span>
-                                        <span class="text-green-500 ml-2">34,000,000 đ</span>
-                                        <span class="text-green-500 ml-2 status-label-2">- 50%</span>
+                                        <span class="text-green-500 ml-2">{booking?.totalPriceAfterTax} VND</span>
+                                        {/*<span class="text-green-500 ml-2">34,000,000 đ</span>
+                                        <span class="text-green-500 ml-2 status-label-2">- 50%</span>*/}
                                     </div>
                                 <div class="flex items-center text-gray-500 mt-1 ">
                                     <FaEnvelope class="mr-2"/>
                                     
                                     <div class="text-gray-500">
-                                    Đặt lúc 15:04 30/07/2024
+                                    Đặt lúc {formattedDate}
                                     </div>
                                 </div>
                             </div>
@@ -77,27 +154,29 @@ const BusinessDetailBookingScreen = () => {
                                     <div class="grid grid-cols-2 gap-4">
                                         <div>
                                             <div class="mb-2 text-gray-500">Mã khách hàng</div>
-                                            <div class="mb-4">124355111</div>
+                                            <div class="mb-4">{userData?._id}</div>
                                             <div class="mb-2 text-gray-500">Số điện thoại</div>
-                                            <div class="mb-4">0987654321</div>
+                                            <div class="mb-4">{userData?.userPhoneNumber}</div>
                                             <div class="mb-2 text-gray-500">Ngày sinh</div>
-                                            <div class="mb-4">12/10/2000</div>
+                                            <div class="mb-4">{userData?.userDateOfBirth}</div>
                                             <div class="mb-2 text-gray-500">Tên liên hệ</div>
-                                            <div class="mb-4">Lê Bảo Như</div>
+                                            <div class="mb-4">{userData?.userName}</div>
                                             <div class="mb-2 text-gray-500">Giới tính</div>
-                                            <div class="mb-4">Nữ</div>
+                                            <div className="mb-4">
+                                                {userData?.gender === 'male' ? 'Nam' : 'Nữ'}
+                                        </div>
                                         </div>
                                         <div>
                                             <div class="mb-2 text-gray-500">Họ và tên</div>
-                                            <div class="mb-4">Nguyễn Thị Xuân Thủy</div>
+                                            <div class="mb-4">{userData?.userName}</div>
                                             <div class="mb-2 text-gray-500">Địa chỉ email</div>
-                                            <div class="mb-4">thuy.ntx@example.com</div>
-                                            <div class="mb-2 text-gray-500">Số CMND/CCCD</div>
-                                            <div class="mb-4">079303041653</div>
+                                            <div class="mb-4">{userData?.userEmail}</div>
+                                            {/* <div class="mb-2 text-gray-500">Số CMND/CCCD</div>
+                                            <div class="mb-4">079303041653</div> */}
                                             <div class="mb-2 text-gray-500">Số điện thoại liên hệ</div>
-                                            <div class="mb-4">0386441295</div>
+                                            <div class="mb-4">{userData?.userPhoneNumber}</div>
                                             <div class="mb-2 text-gray-500">Địa chỉ</div>
-                                            <div class="mb-4">23 Cây Keo, Tam Phú, TP Thủ Đức, TP HCM</div>
+                                            <div class="mb-4">{userData?.userPhoneNumber}</div>
                                         </div>
                                     </div>
                                 </div>
@@ -107,27 +186,29 @@ const BusinessDetailBookingScreen = () => {
                                 <div class="grid grid-cols-2 gap-4">
                                     <div>
                                         <div class="mb-2 text-gray-500">Mã địa điểm</div>
-                                        <div class="mb-4">124355111</div>
-                                        <div class="mb-2 text-gray-500">Tên nhà kinh doanh</div>
-                                        <div class="mb-4">Du lịch Hồ Cốc - Vũng Tàu</div>
+                                        <div class="mb-4">{booking._id}</div>
                                         <div class="mb-2 text-gray-500">Loại</div>
-                                        <div class="mb-4">Camping</div>
-                                        
+                                        <div className="mb-4">
+                                            {
+                                                locationData?.category?.id === 'hotel' ? 'Khách sạn' :
+                                                locationData?.category?.id === 'homestay' ? 'Homestay' :
+                                                locationData?.category?.id === 'guest_home' ? 'Nhà nghỉ' :
+                                                'Danh mục không xác định'
+                                            }
+                                        </div>
                                     </div>
                                     
                                     <div>
                                         <div class="mb-2 text-gray-500">Tên địa điểm</div>
-                                        <div class="mb-4">Du lịch Hồ Cốc - Vũng Tàu</div>
+                                        <div class="mb-4">{locationData.name}</div>
                                         <div class="mb-2 text-gray-500">Địa chỉ</div>
-                                        <div class="mb-4">FFXQ+X94, Bưng Riềng, Xuyên Mộc, Bà Rịa - Vũng Tàu, Vietnam</div>
+                                        <div class="mb-4">{locationData.address}</div>
                                         <div class="mb-2 text-gray-500">Ngày đăng ký kinh doanh</div>
-                                        <div class="mb-4">12/10/2023</div>
+                                        <div class="mb-4">{moment(locationData.dateCreated).format('DD/MM/YYYY HH:mm:ss')}</div>
                                     </div>
                                 </div>
                             </div>
                             )}
-
-
                         </div>
                     </div>
                 </div> 
@@ -137,5 +218,4 @@ const BusinessDetailBookingScreen = () => {
     );
 
 };
-
 export default BusinessDetailBookingScreen;
